718a7fc4b13b832bb4503ed2ccbe8c45684e27f0
Martônio Júnior <martoniojunior@hotmail.com>
2026-01-14 14:13:08 -0300

    remove(Assets): `generatePaths(forResourcesWithExtension:subdirectory:)`

--- Sources/Assets/Assets.swift
+++ Sources/Assets/Assets.swift
@@ -8,11 +8,3 @@
 import Foundation
 
 public let artsBlueprintsCodeBundle = Bundle.module
-
-public func generatePaths(forResourcesWithExtension ext: String, subdirectory subpath: String? = nil) -> [String] {
-    let urls = artsBlueprintsCodeBundle.urls(forResourcesWithExtension: "css", subdirectory: nil) ?? []
-    print(urls)
-    return urls.map {
-        "Resources/EnergyTheme/\($0.relativeString)"
-    }
-}
