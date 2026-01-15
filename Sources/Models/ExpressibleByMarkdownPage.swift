e50e8475b117ac12c5add638dbe34e677acf0503
Martônio Júnior <martoniojunior@hotmail.com>
2026-01-15 18:00:31 -0300

    change(Styleguide): `ExpressibleByMarkdownPage`
    Moved protocol to "Models" module

--- Sources/Models/ExpressibleByMarkdownPage.swift
+++ Sources/Models/ExpressibleByMarkdownPage.swift
@@ -5,7 +5,6 @@
 //  Created by Martônio Júnior on 15/10/2025.
 //
 
-// TODO: Mover este tipo para um módulo dedicado ao processamento Markdown para HTML
 public protocol ExpressibleByMarkdownPage {
     associatedtype Metadata: Decodable
 }
