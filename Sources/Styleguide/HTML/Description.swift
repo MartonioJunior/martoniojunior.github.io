614ff33e1cba0dafcf870a81acad6aaae1682ce2
Martônio Júnior <martoniojunior@hotmail.com>
2026-01-15 19:06:48 -0300

    fix(Styleguide): `Description`
    Removed `descriptionStyle()` in favor of applying class directly

--- Sources/Styleguide/HTML/Description.swift
+++ Sources/Styleguide/HTML/Description.swift
@@ -21,16 +21,6 @@ public struct Description {
 // MARK: Self: HTML.View
 extension Description: HTML.View {
     public var body: some HTML.View {
-        p { text }.descriptionStyle()
-    }
-}
-
-// MARK: HTMLComponent (EX)
-public extension HTMLComponent {
-    func descriptionStyle() -> some HTML.View {
-        `class`(.description)
-        // fontFamily(nil) // Secondary Font
-        // .fontStyle(.italic)
-        // .marginTop(.px(4))
+        p { text }.class(.description)
     }
 }
