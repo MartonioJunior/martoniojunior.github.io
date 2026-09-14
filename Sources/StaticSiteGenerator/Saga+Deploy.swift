c15f74396649302a6cff7bb54258402b66dc73e0
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-14 18:59:56 -0300

    change(StaticSiteGenerator): `ArtsBlueprintsCodeWebsite`
    Adjusted `deploy(_:to:)` implementation

--- Sources/StaticSiteGenerator/Saga+Deploy.swift
+++ Sources/StaticSiteGenerator/Saga+Deploy.swift
@@ -17,19 +17,29 @@ public extension ArtsBlueprintsCodeWebsite {
             .registerProjects(self)
             .blockPages(in: "about/")
             .registerStandalone(self)
-            .afterWrite { _ in
-                try deployFolder("Sources/Assets/Resources", to: "Resources/EnergyTheme")
-                try deployFolder(source + "/assets", to: "assets")
-                try deployFolder("Sources/SagaIntegration/Resources", to: "")
+            .afterWrite {
+                try $0.deployFolder("Sources/Assets/Resources", to: "Resources/EnergyTheme")
+                try $0.deployFolder(source + "/assets", to: "assets")
+                try $0.deployFolder("Sources/SagaIntegration/Resources", to: "")
             }
             .run()
+    }
+}
 
-        // writeIndex(to: destination, posts: [])
-        @Sendable
-        func deployFolder(_ path: String, to targetPath: String) throws {
-            try Folder.current.createSubfolderIfNeeded(at: path).copyFiles(
-                into: .current.subfolder(at: destination).createSubfolderIfNeeded(at: targetPath)
-            )
-        }
+// MARK: Saga (EX)
+public extension Saga {
+    /// Copies the contents of a folder into the deployment folder
+    /// - Parameters:
+    ///   - path: Relative path of what folder will be copied.
+    ///   - outputSubpath: Relative path of where the folder will be pasted.
+    ///   - root: Root used as the reference for the copy-paste operation.
+    ///
+    /// - Throws: Error when any file operation fails
+    func deployFolder(_ path: String, to outputSubpath: String, root: Folder = .current) throws {
+        let relativeOutputPath = try outputPath.relativePath(from: rootPath)
+
+        try root.createSubfolderIfNeeded(at: path).copyFiles(
+            into: root.subfolder(at: relativeOutputPath.string).createSubfolderIfNeeded(at: outputSubpath)
+        )
     }
 }
