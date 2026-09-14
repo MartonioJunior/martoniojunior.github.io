f31bbd66a7b6afbf57f32ec57b1580ac3b8ebe1c
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-14 16:34:46 -0300

    feat(FileClient): `Folder`
    Added `copyFiles(into:)` to the type and remove them from "StaticSiteGenerator" module

--- Sources/FileClient/Extensions/Folder+Utilities.swift
+++ Sources/FileClient/Extensions/Folder+Utilities.swift
@@ -4,3 +4,16 @@
 //
 //  Created by Martônio Júnior on 15/10/2025.
 //
+
+public extension Folder {
+    /// Copies files from this folder into another.
+    /// - Parameter targetFolder: Folder that will receive all the files.
+    /// - Throws: Error when file creation fails.
+    func copyFiles(into targetFolder: Folder) throws {
+        try files.recursive.forEach {
+            let subfolder = $0.path(relativeTo: self)
+            let deployedFile = try targetFolder.createFile(at: subfolder)
+            try deployedFile.write($0.read())
+        }
+    }
+}
--- Sources/StaticSiteGenerator/Saga+Deploy.swift
+++ Sources/StaticSiteGenerator/Saga+Deploy.swift
@@ -5,6 +5,7 @@
 //  Created by Martônio Júnior on 13/10/2025.
 //
 
+import FileClient
 import Models
 import Saga
 
@@ -16,17 +17,17 @@ public extension ArtsBlueprintsCodeWebsite {
             .registerProjects(self)
             .blockPages(in: "about/")
             .registerStandalone(self)
+            .afterWrite { _ in
+                try deployFolder("Sources/Assets/Resources", to: "Resources/EnergyTheme")
+                try deployFolder(source + "/assets", to: "assets")
+                try deployFolder("Sources/SagaIntegration/Resources", to: "")
+            }
             .run()
 
         // writeIndex(to: destination, posts: [])
-
-        try deployFolder("Sources/Assets/Resources", to: "Resources/EnergyTheme")
-        try deployFolder(source + "/assets", to: "assets")
-        try deployFolder("Sources/SagaIntegration/Resources", to: "")
-
+        @Sendable
         func deployFolder(_ path: String, to targetPath: String) throws {
-            try copyFiles(
-                from: .current.createSubfolderIfNeeded(at: path),
+            try Folder.current.createSubfolderIfNeeded(at: path).copyFiles(
                 into: .current.subfolder(at: destination).createSubfolderIfNeeded(at: targetPath)
             )
         }
