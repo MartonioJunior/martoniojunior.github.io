c985a18c258f179bd55979fed5ff74a0cf4668da
Martônio Júnior <martoniojunior@hotmail.com>
2026-09-17 14:45:56 -0300

    feat(HTML): `HighlightJS`
    Added `version` field to control the version of the library to import

--- Sources/Styleguide/HTML/JSLibraries/HighlightJS.swift
+++ Sources/Styleguide/HTML/JSLibraries/HighlightJS.swift
@@ -11,10 +11,12 @@ import Elementary
 /// 
 /// Used to add code syntax highlighting to snippets in the website.
 public struct HighlightJS {
+    /// Version of the library to import.
+    var version: String = "11.11.1"
     /// Languages to be imported.
     var languages: [String] = ["csharp", "swift"]
     /// Should the module import the default CSS for highlight.js?
-    var importDefaultCSS: Bool = true
+    var importDefaultCSS: Bool = false
     /// Instances Highlight.js for use.
     public init() {}
 }
@@ -24,11 +26,11 @@ extension HighlightJS: JSLibrary {
     // swiftlint:disable:next missing_docs
     public var body: some HTML {
         if importDefaultCSS {
-            link.css(path: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/styles/default.min.css")
+            link.css(path: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@\(version)/build/styles/default.min.css")
         }
-        script(.src(.init(stringLiteral: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/highlight.min.js"))) {}
+        script(.src(.init(stringLiteral: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@\(version)/build/highlight.min.js"))) {}
         for language in self.languages {
-            script(.src(.init(stringLiteral: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.11.1/build/languages/\(language).min.js"))) {}
+            script(.src(.init(stringLiteral: "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@\(version)/build/languages/\(language).min.js"))) {}
         }
         script {
             "hljs.highlightAll();"
